f82658827d003b532a0edd5d4078daa2b92b4c1b
rubayetseason <rubayetislamseason5@gmail.com>
2025-04-29 16:37:07 +0600

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'lf' }],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+};
